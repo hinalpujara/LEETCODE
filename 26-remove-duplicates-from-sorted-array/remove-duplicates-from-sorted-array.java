0c92ae1b97130a997e1be5dfcd7333474f145601
Hinal Pujara <hinalpujara26@gmail.com>
2025-07-23 14:16:52 -0400

    Time: 0 ms (100.00%) | Memory: 44.7 MB (79.18%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -5,7 +5,7 @@ class Solution {
             if(nums[i]!=nums[i-1]){
                 nums[k] = nums[i];
                 k++;
-            }     
+            }
         }
         return k;
     }
